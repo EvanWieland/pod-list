949a15801dd76b497c74ae58fddcf95360bf1345
Evan Wieland <evwiel1@gmail.com>
2018-11-30 16:14:19 -0600

    Create pods.js

---
+++ pods.js
@@ -0,0 +1,5 @@
+const desired_pods = [
+    ['happyface', 'http://feeds.megaphone.fm/happyface'],
+    ['up', 'https://rss.art19.com/up-and-vanished'],
+    ['dd', 'https://rss.cast.plus/5b9301029cafaf060e15aec3/rss'],
+];
